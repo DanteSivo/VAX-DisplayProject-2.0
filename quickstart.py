f9cff603f44f7a8f7ff37fc4a14fd803a091a8ad
dvs8452 <dvs8452@g.rit.edu>
2019-11-27 22:12:16 -0500

    Modified getEvents() method to only return up to 3 events, instead of 10.

--- quickstart.py
+++ quickstart.py
@@ -9,9 +9,10 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-def main():
+def getEvents():
     """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
+    Modified by Dante Sivo
+    Returns the data next 3 events on the user's calendar.
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -38,15 +39,21 @@ def main():
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
+                                        maxResults=3, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    return events;
+
+def parseEvents(eventsList):
+    return None
 
 if __name__ == '__main__':
-    main()
+    # Return an array of the next 3 upcoming events. (The Current / Ongoing, and the future 2).
+    eventsList = getEvents()
+    for event in eventsList:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+    parsedEvents = parseEvents(eventsList)
