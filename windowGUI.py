bfe98fbba02538d0c461e60814a473b14b979337
Dante Sivo <dvs8452@rit.edu>
2019-12-29 14:02:51 -0500

    General layout completed. Minor asethic tweaks needed.

--- windowGUI.py
+++ windowGUI.py
@@ -5,12 +5,20 @@ window.title("GUI")
 window.geometry("800x480")
 window.config(bg = '#024889')
 
-eventLabel = Label(window,text=("Event name here"),anchor= 's', height = 10) #set your text
-timeLabel = Label(window,text="Event time here",anchor= 'n', height = 10, pady = 17.5) #set your text
-eventLabel.pack()
-timeLabel.pack()
+photo = PhotoImage(file="images\sprocket150.png")
+w = Label(window, image=photo, bg = '#024889')
+w.photo = photo
 
-eventLabel.config(font=("Courier", 20), fg='#fcd200')
-timeLabel.config(font=("Courier", 20), fg='#fcd200' )
+eventLabel = Label(window,text=("Marvel Mondays! - Endgame"))
+timeLabel = Label(window,text="Event time here")
+
+eventLabel.config(font=("Courier", 26), fg='#fcd200', bg = '#024889')
+timeLabel.config(font=("Courier", 20), fg='#fcd200', bg = '#024889')
+
+#w.grid(row=0, column=0, sticky=N+W, pady=25, padx = 25)
+w.place(x=30, y=30)
+eventLabel.place(x=400,y=225, anchor="center")
+timeLabel.place(x=400,y=275, anchor="center")
+
 
 mainloop()
