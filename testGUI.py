f4ddbebbb502e11edd55579380574c55cee27001
Dante Sivo <dvs8452@rit.edu>
2020-01-04 23:15:55 -0500

    Successfully constructed a update function using a tkinter class.

--- testGUI.py
+++ testGUI.py
@@ -1,15 +1,44 @@
-from tkinter import *
-# Let's create the Tkinter window.
-window = Tk()
+#!/usr/bin/env python3
+
+# Display UTC.
+# started with https://docs.python.org/3.4/library/tkinter.html#module-tkinter
+
+import tkinter as tk
+import time
+
+
+
+def getCount():
+    global globalX
+    globalX *= 2
+    return globalX + 1
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.now = tk.StringVar()
+        self.time = tk.Label(self, font=('Helvetica', 24))
+        self.time.pack(side="top")
+        self.time["textvariable"] = self.now
+
+        # initial time display
+        self.onUpdate()
+
+    def onUpdate(self):
+        # update displayed time
+        self.now.set(getCount())
+        # schedule timer to call myself after 1 second
+        self.after(1000, self.onUpdate)
+
+globalX = 1
+window = tk.Tk()
 window.title("GUI")
 window.geometry("800x480")
+window.config(bg = '#024889')
 
-label = Label(window,text=("Event name here"),anchor= 's', height = 13) #set your text
-label.pack()
-labels.append(label) #appends the label to the list for further use
-
-label2 = Label(window,text="Event time here",anchor= 'n', height = 13) #set your text
-label2.pack()
-labels.append(label2) #appends the label to the list for further use
-
-mainloop()
+app = Application(master=window)
+window.mainloop()
