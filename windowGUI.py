f39ff6d12586672eddc9e02f131f2e52591f031c
Dante Sivo <dvs8452@rit.edu>
2020-01-05 00:39:10 -0500

    Added testing code to main file.

--- windowGUI.py
+++ windowGUI.py
@@ -1,31 +1,53 @@
 from tkinter import *
-import threading
-import time
-# Let's create the Tkinter window.
 
+def getCount():
+    global globalX
+    globalX += 1
+    return globalX
+
+class Application(Frame):
+    def __init__(self, master=None):
+        self.frame = Frame.__init__(self, master, width = 800, height = 480, bg = '#024889')
+        self.pack()
+
+        self.createWidgets()
+
+    def createWidgets(self):
+        # To Be Deleted
+
+        self.photo = PhotoImage(file="images\sprocket150.png")
+        self.w = Label(window, image=self.photo, bg='#024889')
+        self.w.photo = self.photo
+        self.w.place(x=30, y=30)
+
+        self.vaxLabel = Label(self, font=("Tahoma", 26), fg='#fcd200', bg = '#024889', text = "What's going on in the VAX?")
+        self.vaxLabel.place(x=410, y=150, anchor="center")
+
+        self.eventVar = StringVar()
+        self.eventLabel = Label(self, font=("Tahoma", 26), fg='#fcd200', bg = '#024889')
+        self.eventLabel.place(x=400, y=225, anchor="center")
+        self.eventLabel["textvariable"] = self.eventVar
+
+        self.timeVar = StringVar()
+        self.timeLabel = Label(self, font=("Tahoma", 20), fg='#fcd200', bg = '#024889')
+        self.timeLabel.place(x=400,y=275, anchor="center")
+        self.timeLabel["textvariable"] = self.timeVar
+
+        # initial time display
+        self.onUpdate()
+
+    def onUpdate(self):
+        # Updates the display
+        var = getCount()
+        self.eventVar.set(var)
+        self.timeVar.set(var)
+        self.after(1000, self.onUpdate)
+
+globalX = 1
 window = Tk()
 window.title("GUI")
 window.geometry("800x480")
 window.config(bg = '#024889')
 
-eventString = StringVar()
-timeString = StringVar()
-
-photo = PhotoImage(file="images\sprocket150.png")
-w = Label(window, image=photo, bg = '#024889')
-w.photo = photo
-
-eventLabel = Label(window,text=eventString)
-timeLabel = Label(window,text=timeString)
-
-eventLabel.config(font=("Tahoma", 26), fg='#fcd200', bg = '#024889')
-timeLabel.config(font=("Tahoma", 20), fg='#fcd200', bg = '#024889')
-
-#w.grid(row=0, column=0, sticky=N+W, pady=25, padx = 25)
-w.place(x=30, y=30)
-eventLabel.place(x=400,y=225, anchor="center")
-timeLabel.place(x=400,y=275, anchor="center")
-
+app = Application(master=window)
 window.mainloop()
-
-eventString = "derp"
