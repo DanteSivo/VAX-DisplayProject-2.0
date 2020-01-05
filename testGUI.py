f183211b156d9582c6711ae1d8a908c02fef2ae7
Dante Sivo <dvs8452@rit.edu>
2020-01-05 00:30:53 -0500

    Successfully implemented the GUI. (BUG) -> Background GUI plain white, not colored. (NOTE) -> When using Tkinter's place command, use inplace of pack.

--- testGUI.py
+++ testGUI.py
@@ -3,7 +3,7 @@
 # Display UTC.
 # started with https://docs.python.org/3.4/library/tkinter.html#module-tkinter
 
-import tkinter as tk
+from tkinter import *
 import time
 
 
@@ -13,30 +13,30 @@ def getCount():
     globalX += 1
     return globalX
 
-class Application(tk.Frame):
+class Application(Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        self.frame = Frame.__init__(self, master, width = 800, height = 480)
         self.pack()
+
         self.createWidgets()
 
     def createWidgets(self):
         # To Be Deleted
-        self.now = tk.StringVar()
-        self.time = tk.Label(self, font=('Helvetica', 24))
-        #self.time.pack(side="top")
-        self.time["textvariable"] = self.now
 
-        self.eventVar = tk.StringVar()
-        self.eventLabel = tk.Label(self, font=("Tahoma", 26), fg='#fcd200', bg = '#024889')
+        self.photo = PhotoImage(file="images\sprocket150.png")
+        self.w = Label(window, image=self.photo, bg='#024889')
+        self.w.photo = self.photo
+        self.w.place(x=30, y=30)
+
+        self.eventVar = StringVar()
+        self.eventLabel = Label(self, font=("Tahoma", 26), fg='#fcd200', bg = '#024889')
         self.eventLabel.place(x=400, y=225, anchor="center")
-        self.eventLabel.pack()
         self.eventLabel["textvariable"] = self.eventVar
 
-        self.timeVar = tk.StringVar()
-        self.eventLabel = tk.Label(self, font=("Tahoma", 20), fg='#fcd200', bg = '#024889')
-        self.eventLabel.place(x=400,y=275, anchor="center")
-        self.eventLabel.pack()
-        self.eventLabel["textvariable"] = self.timeVar
+        self.timeVar = StringVar()
+        self.timeLabel = Label(self, font=("Tahoma", 20), fg='#fcd200', bg = '#024889')
+        self.timeLabel.place(x=400,y=275, anchor="center")
+        self.timeLabel["textvariable"] = self.timeVar
 
         # initial time display
         self.onUpdate()
@@ -44,14 +44,13 @@ class Application(tk.Frame):
     def onUpdate(self):
         # update displayed time
         var = getCount()
-        self.now.set(var)
         self.eventVar.set(var)
         self.timeVar.set(var)
         # schedule timer to call myself after 1 second
         self.after(1000, self.onUpdate)
 
 globalX = 1
-window = tk.Tk()
+window = Tk()
 window.title("GUI")
 window.geometry("800x480")
 window.config(bg = '#024889')
