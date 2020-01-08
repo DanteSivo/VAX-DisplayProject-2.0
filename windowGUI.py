efa4a8ce8ecedd5c19c7eada255e512bd0824881
Dante Sivo <dvs8452@rit.edu>
2020-01-08 16:14:11 -0500

    Added a clock label to show the current time. Also adjusted the position of different elements on the GUI. Finished the AM/PM parsing of the event time label.

--- windowGUI.py
+++ windowGUI.py
@@ -7,14 +7,20 @@ def updateEvent():
     if (len(eventsList) <= 0):
        return ["No events ongoing or planned!", ""]
    # There must be at least 1 event in the calendar
-    startTime = eventsList[0]['start'].get('dateTime', eventsList[0]['start'].get('date')).split('T') # basic parsing
-    startDate = startTime[0].split('-') # Start date of the event Y/M/D format
-    startTime = startTime[1].split(':') # H/M/timeZone/S format
-    endTime = eventsList[0]['end'].get('dateTime', eventsList[0]['start'].get('date')).split('T') # basic parsing
-    endDate = endTime[0].split('-') # End date of the event Y/M/D format
-    endTime = endTime[1].split('-') # End time of the event
+    start = eventsList[0]['start'].get('dateTime', eventsList[0]['start'].get('date')).split('T') # basic parsing
+    startDate = start[0].split('-') # Start date of the event Y/M/D format
+    startTime = start[1].split(':') # H/M/timeZone/S format
+    end = eventsList[0]['end'].get('dateTime', eventsList[0]['start'].get('date')).split('T') # basic parsing
+    endDate = end[0].split('-') # End date of the event Y/M/D format
+    endTime = end[1].split('-') # End time of the event
     endTime = endTime[0].split(':') # H/M/S format
     eventOngoing = isEventOngoing(startTime, startDate, endTime, endDate)
+
+    getTimeDisplay = timeParse(startTime, endTime)
+
+    if (eventOngoing):
+        # Update label text to show the event in ongoing
+        return ["Right Now: " + eventsList[0].get('summary'), getTimeDisplay]
     return [eventsList, eventsList]
 
 '''isEventOngoing() - A function that determines if an event is going on (TRUE) or if it's coming up (FALSE)
@@ -28,6 +34,26 @@ def isEventOngoing(startTime, startDate, endTime, endDate):
             return True
     return False # All other cases, event has been proven to not be ongoing
 
+'''timeParse() - A function that returns the string to display about how long the event is on for.
+'''
+def timeParse(startTime, endTime):
+    startHour = startTime[0:1]
+    startHour = int(''.join(startHour))
+    if (startHour > 12): # Determine if the Start Time is AM or PM
+        startM = " PM"
+        startHour -= 12
+    else:
+        startM = " AM"
+
+    endHour = endTime[0:1]
+    endHour = int(''.join(endHour))
+    if (endHour > 12): # Determine if the End Time is AM or PM
+        endM = " PM"
+        endHour -= 12
+    else:
+        endM = " AM"
+    return str(startHour) + ':' + startTime[1] + startM  + " - " + str(endHour) + ':' + endTime[1] + endM
+
 class Application(Frame):
     def __init__(self, master=None): # Class constructor
         self.frame = Frame.__init__(self, master, width = 800, height = 480, bg = '#024889')
@@ -39,20 +65,25 @@ class Application(Frame):
         self.photo = PhotoImage(file="images\sprocket150.png")
         self.w = Label(window, image=self.photo, bg='#024889')
         self.w.photo = self.photo
-        self.w.place(x=30, y=30)
+        self.w.place(x=20, y=20)
 
         self.vaxLabel = Label(self, font=("Tahoma", 26), fg='#fcd200', bg = '#024889', text = "What's going on in the VAX?")
         self.vaxLabel.place(x=410, y=150, anchor="center")
 
         self.eventVar = StringVar()
         self.eventLabel = Label(self, font=("Tahoma", 26), fg='#fcd200', bg = '#024889')
-        self.eventLabel.place(x=400, y=225, anchor="center")
+        self.eventLabel.place(x=400, y=250, anchor="center")
         self.eventLabel["textvariable"] = self.eventVar
 
         self.timeVar = StringVar()
         self.timeLabel = Label(self, font=("Tahoma", 20), fg='#fcd200', bg = '#024889')
-        self.timeLabel.place(x=400,y=275, anchor="center")
+        self.timeLabel.place(x=400,y=300, anchor="center")
         self.timeLabel["textvariable"] = self.timeVar
+
+        self.clockVar = StringVar()
+        self.clockLabel = Label(self, font=("Tahoma", 14), fg='#fcd200', bg='#024889')
+        self.clockLabel.place(x=700, y=460, anchor="center")
+        self.clockLabel["textvariable"] = self.clockVar
 
         # initial time display
         self.onUpdate()
@@ -62,6 +93,8 @@ class Application(Frame):
         displayStr = updateEvent()
         self.eventVar.set(displayStr[0])
         self.timeVar.set(displayStr[1])
+        now = datetime.now()
+        self.clockVar.set(now.strftime("%H:%M:%S %m/%d/%Y"))
         self.after(250, self.onUpdate) # Loop update
 
 
