a23d3e85d21fed4146dc7892a73e51433a9e3584
Dante Sivo <dvs8452@rit.edu>
2020-01-08 17:49:30 -0500

    Added minor changes and removed the title bar from the GUI.

--- windowGUI.py
+++ windowGUI.py
@@ -7,10 +7,15 @@ def updateEvent():
     if (len(eventsList) <= 0):
        return ["No events ongoing or planned!", ""]
    # There must be at least 1 event in the calendar
-    start = eventsList[0]['start'].get('dateTime', eventsList[0]['start'].get('date')).split('T') # basic parsing
+
+    event = vaxCheck(eventsList) # Are any of these events going on in the VAX???
+    if (event == None):
+        return ["No events planned", ""]
+
+    start = event['start'].get('dateTime', event['start'].get('date')).split('T') # basic parsing
     startDate = start[0].split('-') # Start date of the event Y/M/D format
     startTime = start[1].split(':') # H/M/timeZone/S format
-    end = eventsList[0]['end'].get('dateTime', eventsList[0]['start'].get('date')).split('T') # basic parsing
+    end = event['end'].get('dateTime', event['start'].get('date')).split('T') # basic parsing
     endDate = end[0].split('-') # End date of the event Y/M/D format
     endTime = end[1].split('-') # End time of the event
     endTime = endTime[0].split(':') # H/M/S format
@@ -22,7 +27,21 @@ def updateEvent():
     if (eventOngoing):
         # Update label text to show the event in ongoing
         return ["Right Now: " + getEventDisplay, getTimeDisplay]
-    return [eventsList, eventsList]
+    # if the event is in the future
+    return ["Next Event: " + getEventDisplay, getTimeDisplay + ' ' + endDate[1] + '/' + endDate[2] + '/' + endDate[0]]
+
+"""vaxCheck() - Determine the soonest event if any, that is in the VAX! Returns the event if found
+"""
+def vaxCheck(eventList):
+
+    for index in range(len(eventList)):
+        summary = eventList[index].get('summary')
+        if ((summary.find("VAX") >= 0) or (summary.find("vax") >= 0) or (summary.find("Vax") >= 0)): # If the word VAX is in the title
+            return eventList[index]
+        location = eventList[index].get('location')
+        if ((location.find("VAX") >= 0) or (location.find("vax") >= 0) or (location.find("Vax") >= 0)):
+            return eventList[index]
+    return None # If no event found within the range - return such
 
 '''isEventOngoing() - A function that determines if an event is going on (TRUE) or if it's coming up (FALSE)
 Parameters - Self descriptive, all passed in from updateEvent()
@@ -91,7 +110,7 @@ class Application(Frame):
 
         self.clockVar = StringVar()
         self.clockLabel = Label(self, font=("Tahoma", 14), fg='#fcd200', bg='#024889')
-        self.clockLabel.place(x=700, y=460, anchor="center")
+        self.clockLabel.place(x=690, y=460, anchor="center")
         self.clockLabel["textvariable"] = self.clockVar
 
         # initial time display
@@ -103,13 +122,19 @@ class Application(Frame):
         self.eventVar.set(displayStr[0])
         self.timeVar.set(displayStr[1])
         now = datetime.now()
-        self.clockVar.set(now.strftime("%H:%M:%S %m/%d/%Y"))
+        hour = int(now.strftime('%H'))
+        if (hour > 12):  # Determine if the Start Time is AM or PM
+            ampm = " PM - "
+            hour -= 12
+        else:
+            ampm = " AM - "
+        self.clockVar.set(str(hour) + ":" + now.strftime("%M:%S") + ampm + now.strftime("%m/%d/%Y"))
         self.after(250, self.onUpdate) # Loop update
-
 
 window = Tk()
 window.title("VAX Reservation Display")
 window.geometry("800x480")
 window.config(bg = '#024889')
+window.overrideredirect(True) # removes title bar
 app = Application(master=window)
 window.mainloop()
