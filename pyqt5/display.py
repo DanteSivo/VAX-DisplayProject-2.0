a5e2f92ea762dda958b80487063bb4ff94525256
Dante Sivo <dvs8452@rit.edu>
2020-03-10 02:06:12 -0400

    Added a loop counter as the label for the lounge to keep track of up-time before crash.

--- pyqt5/display.py
+++ pyqt5/display.py
@@ -8,8 +8,110 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-import time
+from time import sleep
 from threading import Thread
+import quickstart
+from datetime import datetime
+global counterex
+
+def updateEvent():
+    eventsList = quickstart.main()
+    if (len(eventsList) <= 0):
+       return ["No events ongoing or planned!", ""]
+   # There must be at least 1 event in the calendar
+
+    event = eventsList[0] # Are any of these events going on in the VAX???
+    if (event == None):
+        return ["No events planned", ""]
+
+    start = event['start'].get('dateTime', event['start'].get('date')).split('T') # basic parsing
+    startDate = start[0].split('-') # Start date of the event Y/M/D format
+    startTime = start[1].split(':') # H/M/timeZone/S format
+    end = event['end'].get('dateTime', event['start'].get('date')).split('T') # basic parsing
+    endDate = end[0].split('-') # End date of the event Y/M/D format
+    endTime = end[1].split('-') # End time of the event
+    endTime = endTime[0].split(':') # H/M/S format
+    eventOngoing = isEventOngoing(startTime, startDate, endTime, endDate)
+
+    getTimeDisplay = timeParse(startTime, endTime)
+    getEventDisplay = eventParse(eventsList[0].get('summary'))
+
+    if (eventOngoing):
+        # Update label text to show the event in ongoing
+        return ["Right Now: " + getEventDisplay, getTimeDisplay]
+    # if the event is in the future
+    return ["Next Event: " + getEventDisplay, getTimeDisplay + ' ' + endDate[1] + '/' + endDate[2] + '/' + endDate[0]]
+
+def updateUpcoming():
+    eventsList = quickstart.main()
+    if (len(eventsList) <= 2):
+        return ["", ""]
+    # There must be at least 2 events in the calendar
+    event = eventsList[1]
+    if (event == None):
+        return ["No events planned", ""]
+
+    start = event['start'].get('dateTime', event['start'].get('date')).split('T')  # basic parsing
+    startDate = start[0].split('-')  # Start date of the event Y/M/D format
+    startTime = start[1].split(':')  # H/M/timeZone/S format
+    end = event['end'].get('dateTime', event['start'].get('date')).split('T')  # basic parsing
+    endDate = end[0].split('-')  # End date of the event Y/M/D format
+    endTime = end[1].split('-')  # End time of the event
+    endTime = endTime[0].split(':')  # H/M/S format
+    eventOngoing = isEventOngoing(startTime, startDate, endTime, endDate)
+
+    getTimeDisplay = timeParse(startTime, endTime)
+    getEventDisplay = eventParse(eventsList[1].get('summary'))
+
+    if (eventOngoing):
+        # Update label text to show the event in ongoing
+        return ["Also going on: " + getEventDisplay, getTimeDisplay]
+    # if the event is in the future
+    return ["Upcoming: " + getEventDisplay, getTimeDisplay + ' ' + endDate[1] + '/' + endDate[2] + '/' + endDate[0]]
+
+'''isEventOngoing() - A function that determines if an event is going on (TRUE) or if it's coming up (FALSE)
+Parameters - Self descriptive, all passed in from updateEvent()
+Output - FALSE = Event is not ongoing, TRUE = Event is ongoing
+'''
+def isEventOngoing(startTime, startDate, endTime, endDate):
+    now = datetime.now() # %H - Hour, #M - Minute , %S - Second, %D - Date, %m - Month /%d - Day /%Y - Year using .strftime()
+    if ((now.strftime('%Y') >= startDate[0]) & (now.strftime('%m') >= startDate[1]) & (now.strftime('%d') >= startDate[2])): # Check the date, if start was before or at current
+        if ((now.strftime('%H') >= startTime[0]) & (now.strftime('%M') >= startTime[1]) & (now.strftime('%S') >= startTime[3])):
+            return True
+    return False # All other cases, event has been proven to not be ongoing
+
+'''timeParse() - A function that returns the string to display about how long the event is on for.
+'''
+def timeParse(startTime, endTime):
+    startHour = startTime[0:1]
+    startHour = int(''.join(startHour))
+    if (startHour > 12): # Determine if the Start Time is AM or PM
+        startM = " PM"
+        startHour -= 12
+    else:
+        startM = " AM"
+
+    endHour = endTime[0:1]
+    endHour = int(''.join(endHour))
+    if (endHour > 12): # Determine if the End Time is AM or PM
+        endM = " PM"
+        endHour -= 12
+    else:
+        endM = " AM"
+    if (startHour == 0):
+        startHour = 12
+    if (endHour == 0):
+        endHour = 12
+    return str(startHour) + ':' + startTime[1] + startM  + " - " + str(endHour) + ':' + endTime[1] + endM + ' -'
+
+def eventParse(eventName):
+    if (len(eventName) >= 35):
+        if (eventName[34] != ' ' and eventName[35] != ' '):
+            dash = '-'
+        else:
+            dash = ''
+        eventName = eventName[0:35] + dash + '\n' + eventName[35:]
+    return eventName
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -273,9 +375,8 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-        self.i = 0
-        thread = Thread(target=self.count, args=(MainWindow,))
+        self.loop = 0
+        thread = Thread(target=self.update_text, args=(MainWindow,))
         thread.start()
 
     def retranslateUi(self, MainWindow):
@@ -287,15 +388,30 @@ class Ui_MainWindow(object):
         self.upcomingEventLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#fcd200;\">upcomingEventLabel</span></p></body></html>"))
         self.upcomingTimeLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#fcd200;\">upcomingTimeLabel</span></p></body></html>"))
 
-    def count(self, MainWindow):
+    def update_text(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         while True:
-            print(self.i)
+            eventDisplayStr = updateEvent()
+            self.eventLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#fcd200;\">"
+                                                            + eventDisplayStr[0] +
+                                                            "</span></p></body></html>"))
+            self.eventTimeLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#fcd200;\">"
+                                                            + eventDisplayStr[1] +
+                                                            "</span></p></body></html>"))
+            #global counterex
+            upcomingDisplayStr = updateUpcoming()
+            self.upcomingEventLabel.setText(_translate("MainWindow","<html><head/><body><p><span style=\" color:#fcd200;\">"
+                                                            + upcomingDisplayStr[0] +
+                                                            "</span></p></body></html>"))
+            self.upcomingTimeLabel.setText(_translate("MainWindow",
+                                                      "<html><head/><body><p><span style=\" color:#fcd200;\">"
+                                                            + upcomingDisplayStr[1] +
+                                                            "</span></p></body></html>"))
+
             self.vaxLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#fcd200;\">"
-                                                           + str(self.i) +
-                                                           "</span></p></body></html>"))
-            self.i += 1
-            time.sleep(1)
+                                                           + str(self.loop) +"</span></p></body></html>"))
+            self.loop += 1
+            sleep(1)
 
 if __name__ == "__main__":
     import sys
